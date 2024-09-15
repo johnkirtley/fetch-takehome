df72b43bcb140694b61357bdcec042a308177f1a
johnkirtley <kirtleyj16@gmail.com>
2024-09-14 21:06:12 -0700

    added No Filter Results component

---
+++ app/components/NoFilterResults.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import dogImage from '../../public/no-results-doggy.png'
+
+export default function NoFilterResults({ breed }: { breed: string }) {
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-2">
+            <p>No {breed}s found</p>
+            <Image className="opacity-50" src={dogImage} alt="No results" height={200} width={200} />
+        </div>
+    )
+}
