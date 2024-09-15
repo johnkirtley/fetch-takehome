1334abba05ec0cbb7a711ca192ab65fb0911fb09
johnkirtley <kirtleyj16@gmail.com>
2024-09-14 18:02:56 -0700

    setting and getting auth expiration

---
+++ utils/valueExpiration.ts
@@ -0,0 +1,25 @@
+export function setValueExpiration(key: string, value: string, ttl: number) {
+    const now = new Date()
+    const item = {
+        value: value,
+        expiration: now.getTime() + ttl
+    }
+
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+export function getValueExpiration(key: string) {
+    const entry = localStorage.getItem(key);
+
+    if (!entry) return null
+
+    const item = JSON.parse(entry)
+    const now = new Date()
+
+    if (now.getTime() > item.expiration) {
+        localStorage.removeItem(key)
+        return null
+    }
+
+    return item.value
+}
