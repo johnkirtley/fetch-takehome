501d6e9d417b70a88590c9765d7c18d9b79598f5
johnkirtley <kirtleyj16@gmail.com>
2024-09-14 20:34:37 -0700

    Updated to add Dog Card component

--- app/breed/[slug]/page.tsx
+++ app/breed/[slug]/page.tsx
@@ -1,13 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardFooter
-} from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
+import DogCard from '../../components/DogCard';
 
 
 interface Dog {
@@ -21,7 +14,6 @@ interface Dog {
 
 export default function Page({ params }: { params: { slug: string } }) {
     const [breedInfo, setBreedInfo] = useState<Dog[]>([])
-    const [favorites, setFavorites] = useState<string[]>([])
 
     useEffect(() => {
         async function getBreedInfo() {
@@ -64,58 +56,12 @@ export default function Page({ params }: { params: { slug: string } }) {
         getBreedInfo();
     }, [params.slug]);
 
-    const addFavorite = (id: string) => {
-        const currentFavorites = localStorage.getItem('favorites')
-        const parsedFavorites = JSON.parse(currentFavorites || '[]')
-        const newFavorites = [...parsedFavorites, id]
-        localStorage.setItem('favorites', JSON.stringify(newFavorites));
-
-        setFavorites(newFavorites);
-    }
-
-    const removeFavorite = (id: string) => {
-        const currentFavorites = localStorage.getItem('favorites')
-        const parsedFavorites = JSON.parse(currentFavorites || '[]')
-        const updatedFavorites = parsedFavorites.filter((favId: string) => favId !== id)
-        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-        setFavorites(updatedFavorites);
-    }
-
-    const checkFavorites = (id: string) => {
-        const storedFavorites = localStorage.getItem('favorites');
-        const parsedFavorites = JSON.parse(storedFavorites || '[]');
-        return parsedFavorites.includes(id);
-    }
-
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-6">{params.slug.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</h1>
+            <h1 className="text-3xl font-bold mb-6">View All Our Awesome {params.slug.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}s</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {breedInfo.map((dog) => (
-                    <Card key={dog.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-                        <CardContent className="relative h-48">
-                            <Image
-                                src={dog.img}
-                                alt={dog.name}
-                                objectFit="cover"
-                                layout="fill"
-                            />
-                        </CardContent>
-                        <CardHeader>
-                            <h2 className="text-xl font-semibold mb-2">{dog.name}</h2>
-                            <p className="text-gray-600">Age: {dog.age}</p>
-                            <p className="text-gray-600">Zip Code: {dog.zip_code}</p>
-                        </CardHeader>
-                        <CardFooter>
-                            <Button
-                                className={`mt-2 ${checkFavorites(dog.id) ? 'hidden' : 'bg-blue-500 hover:bg-blue-600'}`}
-                                onClick={() => addFavorite(dog.id)}
-                            >
-                                {favorites.includes(dog.id) || checkFavorites(dog.id) ? 'Favorited' : 'Add To Favorites'}
-                            </Button>
-                            {checkFavorites(dog.id) || favorites.includes(dog.id) ? <Button onClick={() => removeFavorite(dog.id)} variant='destructive'>Remove From Favorites</Button> : null}
-                        </CardFooter>
-                    </Card>
+                    <DogCard linkToBreed={false} key={dog.id} dog={dog} />
                 ))}
             </div>
         </div>
