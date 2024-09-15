641ca7899dd32d1e80b04beb0f94fffdacabce65
johnkirtley <kirtleyj16@gmail.com>
2024-09-14 20:34:30 -0700

    Updated to add Dog Card component

--- app/components/BrowseDogs.tsx
+++ app/components/BrowseDogs.tsx
@@ -6,15 +6,25 @@ import { Input } from '@/components/ui/input';
 import { useGlobalState } from '../context/GlobalStateContext';
 import { ChangeEvent } from 'react';
 import ModifySort from './ModifySort';
+import Link from 'next/link';
+import DogCard from './DogCard';
+interface Dog {
+    id: string
+    img: string
+    name: string
+    age: number
+    zip_code: string
+    breed: string
+}
 
 function BrowseDogs() {
     const { availableDogs, setAvailableDogs } = useGlobalState();
-    const [filteredBreeds, setFilteredBreeds] = useState(availableDogs);
+    const [filteredBreeds, setFilteredBreeds] = useState<Dog[]>(availableDogs);
 
     useEffect(() => {
         async function getAvailableDogs() {
             try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dogs/breeds`, {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dogs/search`, {
                     method: 'GET',
                     credentials: 'include',
                 });
@@ -25,8 +35,24 @@ function BrowseDogs() {
 
                 const data = await response.json();
                 console.log('data', data)
-                setAvailableDogs(data);
-                setFilteredBreeds(data); // Set filteredBreeds to the fetched data
+
+                const dogsResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dogs`, {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data.resultIds.slice(0, 99))
+                });
+
+                if (!dogsResponse.ok) {
+                    throw new Error(`HTTP error! status: ${dogsResponse.status}`);
+                }
+
+                const dogsData = await dogsResponse.json()
+                console.log('dogsData', dogsData);
+                setAvailableDogs(dogsData);
+                setFilteredBreeds(dogsData); // Set filteredBreeds to the fetched data
             } catch (error) {
                 console.error('Error fetching dogs:', error);
             }
@@ -41,7 +67,7 @@ function BrowseDogs() {
         if (term === '') {
             setFilteredBreeds(availableDogs);
         } else {
-            const filteredResults = availableDogs.filter((dog) => dog.toLowerCase().includes(term.toLowerCase()));
+            const filteredResults = availableDogs.filter((dog) => dog.breed.toLowerCase().includes(term.toLowerCase()));
             setFilteredBreeds(filteredResults);
         }
     }
@@ -51,11 +77,11 @@ function BrowseDogs() {
             Browse Available Dogs Below
             <Input onChange={handleSearch} />
             <ModifySort filteredBreeds={filteredBreeds} setFilteredBreeds={setFilteredBreeds} />
-            <ul>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {filteredBreeds.map((dog, index) => (
-                    <li key={index}>{dog}</li>
+                    <DogCard linkToBreed={true} dog={dog} key={index} />
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
