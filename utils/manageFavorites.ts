0f539f81dfba4bc1b59e254321f328e997137adb
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 13:42:22 -0700

    added option to get all favorites

--- utils/manageFavorites.ts
+++ utils/manageFavorites.ts
@@ -1,4 +1,4 @@
-export default function manageFavorites(id: string, action: 'add' | 'remove' | 'check') {
+export default function manageFavorites(id?: string, action?: 'add' | 'remove' | 'check' | 'get') {
     let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
 
     if (action === 'add') {
@@ -13,7 +13,11 @@ export default function manageFavorites(id: string, action: 'add' | 'remove' | '
         return favorites.includes(id);
     }
 
+    if (action === 'get') {
+        return favorites;
+    }
+
     localStorage.setItem('favorites', JSON.stringify(favorites));
-    
+
     return favorites;
 }
