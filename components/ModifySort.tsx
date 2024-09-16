695beb03b9661f1a2e4e4d3e20bc2d11a5925ab8
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 09:54:16 -0700

    removed sorting logic. getting sort order from api instead

--- components/ModifySort.tsx
+++ components/ModifySort.tsx
@@ -1,32 +1,11 @@
 import { Button } from './ui/button'
 import React from 'react';
 
-interface Dog {
-    id: string
-    img: string
-    name: string
-    age: number
-    zip_code: string
-    breed: string
-}
-
 export default function ModifySort({
-    filteredBreeds,
-    setFilteredBreeds,
+    setSortType
 }: {
-    filteredBreeds: Dog[];
-    setFilteredBreeds: React.Dispatch<React.SetStateAction<Dog[]>>;
+    setSortType: React.Dispatch<React.SetStateAction<string>>;
 }) {
-
-    const sortAsc = () => {
-        const sortedAsc = filteredBreeds.sort((a: Dog, b: Dog) => a.breed.localeCompare(b.breed));
-        setFilteredBreeds([...sortedAsc]);
-    }
-
-    const sortDesc = () => {
-        const sortedDesc = filteredBreeds.sort((a: Dog, b: Dog) => b.breed.localeCompare(a.breed));
-        setFilteredBreeds([...sortedDesc]);
-    }
 
     return (
         <div className='flex flex-col items-center justify-center gap-2'>
@@ -34,8 +13,8 @@ export default function ModifySort({
                 <p>Sort By Breed:</p>
             </div>
             <div className='flex gap-2'>
-                <Button variant="secondary" onClick={sortAsc}>Asc</Button>
-                <Button variant="secondary" onClick={sortDesc}>Desc</Button>
+                <Button variant="secondary" onClick={() => setSortType('asc')}>Asc</Button>
+                <Button variant="secondary" onClick={() => setSortType('desc')}>Desc</Button>
             </div>
         </div>
     )
