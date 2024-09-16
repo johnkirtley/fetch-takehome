de3b6624c5058f39b0fd846c34004c1b4cc79af3
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 13:41:39 -0700

    added utility function to fetch match and return dog data

---
+++ utils/findMatch.ts
@@ -0,0 +1,35 @@
+export default async function findMatch(favorites: string[]) {
+    const matchResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dogs/match`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(favorites)
+    });
+
+    if (!matchResponse.ok) {
+        throw new Error(`HTTP error! status: ${matchResponse.status}`);
+    }
+
+    const matchData = await matchResponse.json()
+    
+
+    const matchInfo = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dogs`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify([matchData.match])
+    });
+
+    if (!matchInfo.ok) {
+        throw new Error(`HTTP error! status: ${matchInfo.status}`);
+    }
+
+    const matchInfoData = await matchInfo.json();
+    console.log('matchInfoData', matchInfoData);
+
+    return matchInfoData;
+}
