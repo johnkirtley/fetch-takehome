2afd54e4376972f9d9478e93bc7664b4b6f50993
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 09:53:33 -0700

    added to protected routes

--- app/find-my-match/page.tsx
+++ app/find-my-match/page.tsx
@@ -1,7 +1,12 @@
-export default function FindMyMatch() {
+'use client'
+import withAuth from '../../components/ProtectedRoute';
+
+function FindMyMatch() {
     return (
         <div>
             <h1>Find My Match</h1>
         </div>
     )
 }
+
+export default withAuth(FindMyMatch);
