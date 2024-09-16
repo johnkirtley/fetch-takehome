cef26e00c55929304b3cdfe1a19887857b8bf6a3
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 14:54:53 -0700

    added find my match page

--- app/find-my-match/page.tsx
+++ app/find-my-match/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-
 import withAuth from '../../components/ProtectedRoute';
+import Link from 'next/link';
 import manageFavorites from '../../utils/manageFavorites';
 import { Button } from '../../components/ui/button';
 import findMatch from '../../utils/findMatch';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DogCard from '../../components/DogCard';
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
@@ -19,22 +19,37 @@ interface Dog {
 function FindMyMatch() {
     const [match, setMatch] = useState<Dog | null>(null);
     const [showConfetti, setShowConfetti] = useState(false);
+    const [favoritesExist, setFavoritesExist] = useState(false);
     const { width, height } = useWindowSize();
+
+    useEffect(() => {
+        const allFavorites = manageFavorites('get');
+        setFavoritesExist(allFavorites.length > 0);
+    }, []);
 
     const searchForMatch = async () => {
         const allFavorites = manageFavorites('get');
+
+        if (allFavorites.length === 0) {
+            setFavoritesExist(false);
+            return;
+        }
+
         const match = await findMatch(allFavorites);
-        console.log('match', match);
         setMatch(match[0]);
+
         setShowConfetti(true);
         setTimeout(() => {
             setShowConfetti(false);
         }, 7000);
     }
+
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <h1 className='text-2xl lg:text-4xl text-center font-bold mt-5'>Ready to find your best match?</h1>
-            <Button className='my-5' onClick={searchForMatch}>I&apos;m ready! Find my match</Button>
+        <div className='flex flex-col items-center justify-center p-8'>
+            <h1 className='text-2xl text-center font-bold mt-5'>Ready to find your new best friend?</h1>
+            <div className='my-5'>
+                {favoritesExist ? <Button onClick={searchForMatch}>Find my match</Button> : <Button variant="secondary"><Link href="/">Add some favorites first</Link></Button>}
+            </div>
             {showConfetti && <Confetti width={width} height={height} />}
             {match && <DogCard dog={match} linkToBreed={true} />}
         </div>
