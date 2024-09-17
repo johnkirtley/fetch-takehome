e271b843bdc82af887860defb7d2b0509231af94
johnkirtley <kirtleyj16@gmail.com>
2024-09-16 21:07:29 -0700

    refactored for readability

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
 import { useRouter } from "next/navigation";
+
 import { getValueExpiration } from "../utils/valueExpiration";
 
 function useAuthCheck() {
